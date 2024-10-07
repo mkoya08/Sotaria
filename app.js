4ac527ae0d5a7da7c672a6b31decf8824f23e3ee
Moyosore Ogunkoya <momoart188@gmail.com>
2024-10-06 23:41:48 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const menu = document.querySelector('#mobile-menu'); 
+const menuLinks = document.querySelector('.navbar__menu'); 
+
+menu.addEventListener('click', function() { /*Once clicked functionruns*/
+    menu.classList.toggle('is-active'); 
+    menuLinks.classList.toggle('active'); 
+}); 
+
+let audioPlayer = document.getElementById('audioPlayer');
+function play(){
+  audioPlayer.load(); // reload the audio player so that the audio restarts from the beginning on each click
+  audioPlayer.play();
+}
+
+let nextBtn = document.querySelector('.next')
+let prevBtn = document.querySelector('.prev')
+
+let slider = document.querySelector('.slider')
+let sliderList = slider.querySelector('.slider .list')
+let thumbnail = document.querySelector('.slider .thumbnail')
+let thumbnailItems = thumbnail.querySelectorAll('.item')
+
+thumbnail.appendChild(thumbnailItems[0])
+
+// Function for next button 
+nextBtn.onclick = function() {
+    moveSlider('next')
+}
+
+
+// Function for prev button 
+prevBtn.onclick = function() {
+    moveSlider('prev')
+}
+
+
+function moveSlider(direction) {
+    let sliderItems = sliderList.querySelectorAll('.item')
+    let thumbnailItems = document.querySelectorAll('.thumbnail .item')
+    
+    if(direction === 'next'){
+        sliderList.appendChild(sliderItems[0])
+        thumbnail.appendChild(thumbnailItems[0])
+        slider.classList.add('next')
+    } else {
+        sliderList.prepend(sliderItems[sliderItems.length - 1])
+        thumbnail.prepend(thumbnailItems[thumbnailItems.length - 1])
+        slider.classList.add('prev')
+    }
+
+
+    slider.addEventListener('animationend', function() {
+        if(direction === 'next'){
+            slider.classList.remove('next')
+        } else {
+            slider.classList.remove('prev')
+        }
+    }, {once: true}) // Remove the event listener after it's triggered once
+}
+
